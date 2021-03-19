89fb94398a35c310c7762b90392570bd9bc2bba6
abzechix <abderrahimzeghichi@gmail.com>
2021-03-19 04:34:56 +0100

    cloudinary

---
+++ config/plugins.js
@@ -0,0 +1,17 @@
+module.exports = ({ env }) => ({
+    // ...
+    upload: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: 'domgjuzyk',
+        api_key: '675756175582725',
+        api_secret: 'ThPrTAQZiBMTeabV-iqoc8g_OLQ',
+      },
+    },
+    // ...
+  });
+
+
+//   cloud_name: 'domgjuzyk',
+//         api_key: '675756175582725',
+//         api_secret: 'ThPrTAQZiBMTeabV-iqoc8g_OLQ',
