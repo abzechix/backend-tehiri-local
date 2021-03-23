67d23a8680158c2bbb0239e857e63b424d16f270
abzechix <abderrahimzeghichi@gmail.com>
2021-03-23 15:28:51 +0100

    quovery sos-tehiri

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: '0.0.0.0',
-  port: 1337,
+  host: env('APP_HOST', '0.0.0.0'),
+  port: env.int('NODE_PORT', 1337),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'ad4f3bbfa3912effa5e8668dfa4772c5'),
